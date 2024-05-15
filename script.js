d4c0c721e5a243c1b50f23d7f10234fa4f4fc7eb
hhansy <maksik30082008@gmail.com>
2024-05-15 20:16:07 +0300

    змінив фон

---
+++ script.js
@@ -0,0 +1,6 @@
+let modal_btn=document.querySelector("#add_aply")
+let modal=document.querySelector(".modal-bg")
+
+modal_btn.addEventListener("click",function(){
+    modal.style.display="block"
+})
